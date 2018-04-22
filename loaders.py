0e82e6bdbabc2eb256c27518ce5b6142db4e2e02
Alex Gidiotis <alexgidiotis@Alexs-Air.lan>
2018-04-23 00:27:22 +0300

    added loaders for 10 datasets

---
+++ loaders.py
@@ -0,0 +1,465 @@
+import sys
+
+import numpy as np
+import pandas as pd
+
+from sklearn.datasets import fetch_20newsgroups ,fetch_rcv1
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+
+from keras.datasets import imdb
+
+
+def load_20news():
+	"""
+	"""
+	print 'Loading data...'
+	newsgroups_train = fetch_20newsgroups(subset='train',
+		remove=('headers', 'footers', 'quotes'),
+		shuffle=True)
+	newsgroups_test = fetch_20newsgroups(subset='test',
+		remove=('headers', 'footers', 'quotes'),
+		shuffle=True)
+
+	print 'Preprocessing...'
+	vectorizer = TfidfVectorizer(strip_accents='unicode',
+		lowercase=True,
+		stop_words='english',
+		ngram_range=(1, 2),
+		max_df=0.5,
+		min_df=5,
+		max_features=20000,
+		norm='l2',
+		use_idf=True,
+		smooth_idf=True,
+		sublinear_tf=False)
+
+	vectorizer.fit(newsgroups_train.data)
+
+	X_train = vectorizer.transform(newsgroups_train.data)
+	y_train = newsgroups_train.target
+	X_test = vectorizer.transform(newsgroups_test.data)
+	y_test = newsgroups_test.target
+
+	X_train, X_val, y_train, y_val = train_test_split(X_train,
+		y_train,
+		test_size=0.2,
+		random_state=0)
+
+	return X_train, y_train, X_val, y_val, X_test, y_test
+
+
+def load_imdb():
+	"""
+	"""
+	print 'Loading data...'
+
+	word_to_index = imdb.get_word_index()
+	index_to_word = [None] * (max(word_to_index.values()) + 1)
+	for w, i in word_to_index.items():
+		index_to_word[i] = w
+
+	(X_train, y_train), (X_test, y_test) = imdb.load_data()
+
+	print 'Preprocessing...'
+	X_train = [
+		' '.join(index_to_word[i]
+			for i in X_train[i]
+			if i < len(index_to_word))
+		for i in range(X_train.shape[0])
+	]
+
+	X_test = [
+		' '.join(index_to_word[i]
+			for i in X_test[i]
+			if i < len(index_to_word)) 
+		for i in range(X_test.shape[0])
+	]
+
+	vectorizer = TfidfVectorizer(strip_accents='unicode',
+		lowercase=True,
+		stop_words='english',
+		ngram_range=(1, 2),
+		max_df=0.5,
+		min_df=5,
+		max_features=50000,
+		norm='l2',
+		use_idf=True,
+		smooth_idf=True,
+		sublinear_tf=False)
+
+	vectorizer.fit(X_train)
+
+	X_train = vectorizer.transform(X_train)
+	X_test = vectorizer.transform(X_test)
+
+
+	X_train, X_val, y_train, y_val = train_test_split(X_train,
+		y_train,
+		test_size=0.2,
+		random_state=0)
+
+	return X_train, y_train, X_val, y_val, X_test, y_test
+
+
+def load_sms():
+	"""
+	"""
+	print 'Loading data...'
+
+	df = pd.read_csv('data/smsspamcollection/SMSSpamCollection',
+		header=None,
+		delimiter='\t')
+
+	classes = dict((k,idx) for idx,k in enumerate(df[0].unique()))
+	y_data = df[0].map(lambda x: classes[x]).tolist()
+	X_data = df[1].tolist()
+
+	vectorizer = TfidfVectorizer(strip_accents='unicode',
+		lowercase=True,
+		stop_words='english',
+		ngram_range=(1, 2),
+		max_df=0.5,
+		min_df=5,
+		max_features=50000,
+		norm='l2',
+		use_idf=True,
+		smooth_idf=True,
+		sublinear_tf=False)
+
+	vectorizer.fit(X_data)
+	X_data = vectorizer.transform(X_data)
+
+	X_train, X_test, y_train, y_test = train_test_split(X_data,
+		y_data,
+		test_size=0.1,
+		random_state=0)
+
+	X_train, X_val, y_train, y_val = train_test_split(X_train,
+		y_train,
+		test_size=0.2,
+		random_state=0)
+
+	return X_train, y_train, X_val, y_val, X_test, y_test
+
+
+def load_paper_reviews():
+	"""
+	"""
+	df = pd.read_json('data/reviews.json')
+	class2id = dict((k,idx) for idx,k in enumerate(df['preliminary_decision'].unique()))
+
+	y_data = df['preliminary_decision'].map(lambda x: class2id[x]).tolist()
+
+
+	X_data = df['review']
+	X_list = []
+	y_list = []
+
+	for i,(review,lab) in enumerate(zip(X_data,y_data)):
+		try:
+			X_list.append(review[0]['text'])
+			y_list.append(lab)
+		except:
+			continue
+	
+	y_data = y_list
+
+	print 'Preprocessing...'
+	vectorizer = TfidfVectorizer(strip_accents='unicode',
+		lowercase=True,
+		stop_words='english',
+		ngram_range=(1, 2),
+		max_df=0.5,
+		min_df=5,
+		max_features=10000,
+		norm='l2',
+		use_idf=True,
+		smooth_idf=True,
+		sublinear_tf=False)
+
+	vectorizer.fit(X_list)
+
+	X_data = vectorizer.transform(X_list)
+	
+	X_train, X_test, y_train, y_test = train_test_split(X_data,
+		y_data,
+		test_size=0.1,
+		random_state=0)
+
+	X_train, X_val, y_train, y_val = train_test_split(X_train,
+		y_train,
+		test_size=0.2,
+		random_state=0)
+
+	return X_train, y_train, X_val, y_val, X_test, y_test
+
+
+def load_yelp():
+	"""
+	"""
+	df = pd.read_json('data/yelp.json',orient='records',lines=True)
+
+	X_data = df['text'].tolist()
+	y_data = df['stars'].tolist()
+
+	print 'Preprocessing...'
+	vectorizer = TfidfVectorizer(strip_accents='unicode',
+		lowercase=True,
+		stop_words='english',
+		ngram_range=(1, 2),
+		max_df=0.5,
+		min_df=5,
+		max_features=20000,
+		norm='l2',
+		use_idf=True,
+		smooth_idf=True,
+		sublinear_tf=False)
+
+	vectorizer.fit(X_data)
+
+	X_data = vectorizer.transform(X_data)
+	
+	X_train, X_test, y_train, y_test = train_test_split(X_data,
+		y_data,
+		test_size=0.1,
+		random_state=0)
+
+	X_train, X_val, y_train, y_val = train_test_split(X_train,
+		y_train,
+		test_size=0.2,
+		random_state=0)
+
+	return X_train, y_train, X_val, y_val, X_test, y_test
+
+
+def load_amazon():
+	"""
+	"""
+	df = pd.read_csv('data/amazon.txt',
+		header=None,
+		delimiter='\t')
+
+	X_data = df[0].tolist()
+	y_data = df[1].tolist()
+
+	print 'Preprocessing...'
+	vectorizer = TfidfVectorizer(strip_accents='unicode',
+		lowercase=True,
+		stop_words='english',
+		ngram_range=(1, 2),
+		max_df=0.5,
+		min_df=5,
+		max_features=20000,
+		norm='l2',
+		use_idf=True,
+		smooth_idf=True,
+		sublinear_tf=False)
+
+	vectorizer.fit(X_data)
+
+	X_data = vectorizer.transform(X_data)
+	
+	X_train, X_test, y_train, y_test = train_test_split(X_data,
+		y_data,
+		test_size=0.1,
+		random_state=0)
+
+	X_train, X_val, y_train, y_val = train_test_split(X_train,
+		y_train,
+		test_size=0.2,
+		random_state=0)
+
+	return X_train, y_train, X_val, y_val, X_test, y_test
+
+
+def load_youtube():
+	"""
+	"""
+	df = pd.read_csv('data/untitled1/Youtube01-Psy.csv')
+	df = df.append(pd.read_csv('data/untitled1/Youtube02-KatyPerry.csv'))
+	df = df.append(pd.read_csv('data/untitled1/Youtube03-LMFAO.csv'))
+	df = df.append(pd.read_csv('data/untitled1/Youtube04-Eminem.csv'))
+	df = df.append(pd.read_csv('data/untitled1/Youtube05-Shakira.csv'))
+
+	X_data = df["CONTENT"].tolist()
+	y_data = df["CLASS"].tolist()
+
+
+	print 'Preprocessing...'
+	vectorizer = TfidfVectorizer(strip_accents='unicode',
+		lowercase=True,
+		stop_words='english',
+		ngram_range=(1, 2),
+		max_df=0.5,
+		min_df=5,
+		max_features=20000,
+		norm='l2',
+		use_idf=True,
+		smooth_idf=True,
+		sublinear_tf=False)
+
+	vectorizer.fit(X_data)
+
+	X_data = vectorizer.transform(X_data)
+	
+	X_train, X_test, y_train, y_test = train_test_split(X_data,
+		y_data,
+		test_size=0.1,
+		random_state=0)
+
+	X_train, X_val, y_train, y_val = train_test_split(X_train,
+		y_train,
+		test_size=0.2,
+		random_state=0)
+
+	return X_train, y_train, X_val, y_val, X_test, y_test
+
+
+def load_reuters8():
+	"""
+	"""
+	
+	df = pd.read_csv('data/r8-train-all-terms.txt',
+		header=None,
+		delimiter='\t')
+
+	test_df = pd.read_csv('data/r8-test-all-terms.txt',
+		header=None,
+		delimiter='\t')
+
+	class2id = dict((k,idx) for idx,k in enumerate(df[0].unique()))
+
+	X_train = df[1].tolist()
+	X_test = df[1].tolist()
+
+	y_train = df[0].map(lambda x: class2id[x]).tolist()
+	y_test = test_df[0].map(lambda x: class2id[x]).tolist()
+
+	print 'Preprocessing...'
+	vectorizer = TfidfVectorizer(strip_accents='unicode',
+		lowercase=True,
+		stop_words='english',
+		ngram_range=(1, 2),
+		max_df=0.5,
+		min_df=5,
+		max_features=20000,
+		norm='l2',
+		use_idf=True,
+		smooth_idf=True,
+		sublinear_tf=False)
+
+	vectorizer.fit(X_train)
+
+	X_train = vectorizer.transform(X_train)
+	X_test = vectorizer.transform(X_test)
+
+	X_train, X_val, y_train, y_val = train_test_split(X_train,
+		y_train,
+		test_size=0.2,
+		random_state=0)
+
+	return X_train, y_train, X_val, y_val, X_test, y_test
+
+
+def load_reuters52():
+	"""
+	"""
+	
+	df = pd.read_csv('data/r52-train-all-terms.txt',
+		header=None,
+		delimiter='\t')
+
+	test_df = pd.read_csv('data/r52-test-all-terms.txt',
+		header=None,
+		delimiter='\t')
+
+	class2id = dict((k,idx) for idx,k in enumerate(df[0].unique()))
+
+	X_train = df[1].tolist()
+	X_test = df[1].tolist()
+
+	y_train = df[0].map(lambda x: class2id[x]).tolist()
+	y_test = test_df[0].map(lambda x: class2id[x]).tolist()
+
+	print 'Preprocessing...'
+	vectorizer = TfidfVectorizer(strip_accents='unicode',
+		lowercase=True,
+		stop_words='english',
+		ngram_range=(1, 2),
+		max_df=0.5,
+		min_df=5,
+		max_features=20000,
+		norm='l2',
+		use_idf=True,
+		smooth_idf=True,
+		sublinear_tf=False)
+
+	vectorizer.fit(X_train)
+
+	X_train = vectorizer.transform(X_train)
+	X_test = vectorizer.transform(X_test)
+
+	X_train, X_val, y_train, y_val = train_test_split(X_train,
+		y_train,
+		test_size=0.2,
+		random_state=0)
+
+	return X_train, y_train, X_val, y_val, X_test, y_test
+
+
+def load_webkb():
+	"""
+	"""
+	
+	df = pd.read_csv('data/web.txt',
+		header=None,
+		delimiter='\t')
+
+	class2id = dict((k,idx) for idx,k in enumerate(df[0].unique()))
+
+	y_data = df[0].map(lambda x: class2id[x]).tolist()
+	X_list = df[1].tolist()
+
+
+	X_data = []
+	y_list = []
+	for x,y in zip(X_list,y_data):
+		try:
+			if np.isnan(x):
+				continue
+		except:
+			pass
+		X_data.append(x)
+		y_list.append(y)
+
+	y_data = y_list
+
+	print 'Preprocessing...'
+	vectorizer = TfidfVectorizer(strip_accents='unicode',
+		lowercase=True,
+		stop_words='english',
+		ngram_range=(1, 2),
+		max_df=0.5,
+		min_df=5,
+		max_features=10000,
+		norm='l2',
+		use_idf=True,
+		smooth_idf=True,
+		sublinear_tf=False)
+
+	vectorizer.fit(X_data)
+
+	X_data = vectorizer.transform(X_data)
+	
+	X_train, X_test, y_train, y_test = train_test_split(X_data,
+		y_data,
+		test_size=0.1,
+		random_state=0)
+
+	X_train, X_val, y_train, y_val = train_test_split(X_train,
+		y_train,
+		test_size=0.2,
+		random_state=0)
+
+	return X_train, y_train, X_val, y_val, X_test, y_test
--- text_model.py
+++ text_model.py
@@ -1,121 +1,68 @@
 import sys
 
 import numpy as np
+import pandas as pd
 
-from sklearn.datasets import fetch_20newsgroups ,fetch_rcv1
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import BaggingClassifier, AdaBoostClassifier, RandomForestClassifier, GradientBoostingClassifier
 from sklearn.naive_bayes import MultinomialNB
 from sklearn import metrics
 
-from sklearn.svm import SVC
-from keras.datasets import imdb
-
-
-def load_20news():
-	"""
-	"""
-	print 'Loading data...'
-	newsgroups_train = fetch_20newsgroups(subset='train',
-		remove=('headers', 'footers', 'quotes'),
-		shuffle=True)
-	newsgroups_test = fetch_20newsgroups(subset='test',
-		remove=('headers', 'footers', 'quotes'),
-		shuffle=True)
-
-	print 'Preprocessing...'
-	vectorizer = TfidfVectorizer(strip_accents='unicode',
-		lowercase=True,
-		stop_words='english',
-		ngram_range=(1, 2),
-		max_df=0.5,
-		min_df=5,
-		max_features=20000,
-		norm='l2',
-		use_idf=True,
-		smooth_idf=True,
-		sublinear_tf=False)
-
-	vectorizer.fit(newsgroups_train.data)
-
-	X_train = vectorizer.transform(newsgroups_train.data)
-	y_train = newsgroups_train.target
-	X_test = vectorizer.transform(newsgroups_test.data)
-	y_test = newsgroups_test.target
-
-	X_train, X_val, y_train, y_val = train_test_split(X_train,
-		y_train,
-		test_size=0.2,
-		random_state=0)
-
-	return X_train, y_train, X_val, y_val, X_test, y_test
-
-
-def load_imdb():
-	"""
-	"""
-	print 'Loading data...'
-
-	word_to_index = imdb.get_word_index()
-	index_to_word = [None] * (max(word_to_index.values()) + 1)
-	for w, i in word_to_index.items():
-		index_to_word[i] = w
-
-	(X_train, y_train), (X_test, y_test) = imdb.load_data()
-
-	print 'Preprocessing...'
-	X_train = [
-		' '.join(index_to_word[i]
-			for i in X_train[i]
-			if i < len(index_to_word))
-		for i in range(X_train.shape[0])
-	]
-
-	X_test = [
-		' '.join(index_to_word[i]
-			for i in X_test[i]
-			if i < len(index_to_word)) 
-		for i in range(X_test.shape[0])
-	]
-
-	vectorizer = TfidfVectorizer(strip_accents='unicode',
-		lowercase=True,
-		stop_words='english',
-		ngram_range=(1, 2),
-		max_df=0.5,
-		min_df=5,
-		max_features=50000,
-		norm='l2',
-		use_idf=True,
-		smooth_idf=True,
-		sublinear_tf=False)
-
-	vectorizer.fit(X_train)
-
-	X_train = vectorizer.transform(X_train)
-	X_test = vectorizer.transform(X_test)
-
-
-	X_train, X_val, y_train, y_val = train_test_split(X_train,
-		y_train,
-		test_size=0.2,
-		random_state=0)
-
-	return X_train, y_train, X_val, y_val, X_test, y_test
-
-
+from loaders import load_20news,load_imdb,load_sms,load_amazon,load_paper_reviews
+from loaders import load_yelp,load_youtube,load_reuters8,load_reuters52,load_webkb
+	
 
 if __name__ == '__main__':
 	"""
 	20 newsgroup: 
 		Bagging: max_samples=0.8, max_features=0.7
 		AdaBoost: n_estimators=300, learning_rates=1.7
+		GradientBoostingClassifier: estimator_nums=, learning_rates=, max_depths=
 
 	IMDB:
 		Bagging: max_samples=0.7, max_features=0.95, n_estimators=40
 		AdaBoost: n_estimators=200, learning_rates=1.0
+		GradientBoostingClassifier: estimator_nums=, learning_rates=, max_depths=
+
+	SMSSpamCollection:
+		Bagging: max_samples=0.4, max_features=0.6, n_estimators=60
+		AdaBoost: n_estimators=30, learning_rates=1.5
+		GradientBoostingClassifier: estimator_nums=100, learning_rates=0.5, max_depths=3
+
+	paper reviews:
+		Bagging: max_samples=0.4, max_features=0.8, n_estimators=20
+		AdaBoost: n_estimators=10, learning_rates=0.3
+		GradientBoostingClassifier: estimator_nums=100, learning_rates=1.0, max_depths=2
+
+	yelp:
+		Bagging: max_samples=0.3, max_features=0.95, n_estimators=70
+		AdaBoost: n_estimators=60, learning_rates=1.5
+		GradientBoostingClassifier: estimator_nums=, learning_rates=, max_depths=
+
+	amazon:
+		Bagging: max_samples=0.3, max_features=0.5, n_estimators=20
+		AdaBoost: n_estimators=30, learning_rates=0.5
+		GradientBoostingClassifier: estimator_nums=50, learning_rates=0.5, max_depths=7
+
+	youtube:
+		Bagging: max_samples=0.3, max_features=0.6, n_estimators=10
+		AdaBoost: n_estimators=10, learning_rates=0.5
+		GradientBoostingClassifier: estimator_nums=50, learning_rates=0.7, max_depths=2
+
+	reuters8:
+		Bagging: max_samples=0.5, max_features=0.9, n_estimators=100
+		AdaBoost: n_estimators=100, learning_rates=1.2
+		GradientBoostingClassifier: estimator_nums=, learning_rates=, max_depths=
+
+	reuters52:
+		Bagging: max_samples=0.95, max_features=0.95, n_estimators=50
+		AdaBoost: n_estimators=250, learning_rates=1.0
+		GradientBoostingClassifier: estimator_nums=, learning_rates=, max_depths=
+
+	reuterswebkb:
+		Bagging: max_samples=0.7, max_features=0.5, n_estimators=100
+		AdaBoost: n_estimators=50, learning_rates=0.95
+		GradientBoostingClassifier: estimator_nums=, learning_rates=, max_depths=
 	"""
 
 	dataset = sys.argv[1]
@@ -124,6 +71,22 @@ if __name__ == '__main__':
 		X_train, y_train, X_val, y_val, X_test, y_test = load_20news()
 	elif dataset == 'imdb':
 		X_train, y_train, X_val, y_val, X_test, y_test = load_imdb()
+	elif dataset == 'sms':
+		X_train, y_train, X_val, y_val, X_test, y_test = load_sms()
+	elif dataset == 'p_reviews':
+		X_train, y_train, X_val, y_val, X_test, y_test = load_paper_reviews()
+	elif dataset == 'yelp':
+		X_train, y_train, X_val, y_val, X_test, y_test = load_yelp()
+	elif dataset == 'amazon':
+		X_train, y_train, X_val, y_val, X_test, y_test = load_amazon()
+	elif dataset == 'youtube':
+		X_train, y_train, X_val, y_val, X_test, y_test = load_youtube()
+	elif dataset == 'r8':
+		X_train, y_train, X_val, y_val, X_test, y_test = load_reuters8()
+	elif dataset == 'r52':
+		X_train, y_train, X_val, y_val, X_test, y_test = load_reuters52()
+	elif dataset == 'webkb':
+		X_train, y_train, X_val, y_val, X_test, y_test = load_webkb()
 
 
 	clf = MultinomialNB(alpha=.01)
@@ -134,10 +97,10 @@ if __name__ == '__main__':
 	val_preds = clf.predict(X_val)
 	print 'NB training f-score:',metrics.f1_score(y_train, preds, average='macro')
 	print 'NB test f-score:',metrics.f1_score(y_val, val_preds, average='macro')
-
-	estimator_nums = [40]
+	
+	estimator_nums = [100]
 	max_samps = [0.7]
-	max_feats = [0.95]
+	max_feats = [0.5]
 	best_fscore = 0.0
 	for m in max_samps:
 		for n in estimator_nums:
@@ -161,9 +124,10 @@ if __name__ == '__main__':
 	val_preds = best_clf.predict(X_val)
 	print 'Bagging training f-score:',metrics.f1_score(y_train, preds, average='macro')
 	print 'Bagging test f-score:',metrics.f1_score(y_val, val_preds, average='macro')
-
-	estimator_nums = [200]
-	learning_rates = [1.0]
+	
+	
+	estimator_nums = [50]
+	learning_rates = [0.95]
 	best_fscore = 0.0
 	for n in estimator_nums:
 		for lr in learning_rates:
@@ -185,7 +149,33 @@ if __name__ == '__main__':
 	val_preds = best_clf.predict(X_val)
 	print 'AdaBoost training f-score:',metrics.f1_score(y_train, preds, average='macro')
 	print 'AdaBoost test f-score:',metrics.f1_score(y_val, val_preds, average='macro')
-
 	
+	
+	estimator_nums = [50,100,150]
+	learning_rates = [0.5,0.7,1.0,1.5]
+	max_depths = [2,5,7]
+	best_fscore = 0.0
+	for n in estimator_nums:
+		for lr in learning_rates:
+			for d in max_depths:
+				gb_clf = GradientBoostingClassifier(n_estimators=n,
+					max_depth=d,
+					learning_rate=lr,
+					random_state=0)
+				gb_clf.fit(X_train, y_train)
+				
+				val_preds = gb_clf.predict(X_val)
+				val_score = metrics.f1_score(y_val, val_preds, average='macro')
+				if val_score > best_fscore:
+					best_fscore = val_score
+					best_params = (n,lr,d)
+					best_clf = gb_clf
 
+	print 'best parameters:',best_params
+	preds = best_clf.predict(X_train)
+	val_preds = best_clf.predict(X_val)
+	print 'Gradient Boosting training f-score:',metrics.f1_score(y_train, preds, average='macro')
+	print 'Gradient Boosting test f-score:',metrics.f1_score(y_val, val_preds, average='macro')
+	
+	
 
