a1063464485ebb6a2b42e429cff753d64345766b
Alex Gidiotis <gidiotisAlex@outlook.com.gr>
2018-05-12 17:38:52 +0300

    added a multilabel version of fasttext for deliciousMIL

---
+++ delicious_loader.py
@@ -0,0 +1,147 @@
+import pandas as pd
+import numpy as np
+import re
+import time
+
+
+from keras.preprocessing import sequence
+
+
+
+
+
+def read_data(file,
+	lab_file):
+	"""
+	"""
+
+	X_data = pd.read_csv(file,header=None)
+	y_data = pd.read_csv(lab_file,header=None)
+	X_data = X_data[0].map(lambda x: re.sub('<\d+>','',x) \
+		.strip() \
+		.split())
+	X_data = X_data.map(lambda x: [int(tok.strip()) for tok in x])
+	y_data = y_data[0].map(lambda x: np.array([int(lab) for lab in x.split()]))
+
+	return X_data.tolist(),np.array(y_data.tolist())
+
+
+
+
+
+
+def create_ngram_set(input_list, ngram_value=2):
+    """
+    Extract a set of n-grams from a list of integers.
+    >>> create_ngram_set([1, 4, 9, 4, 1, 4], ngram_value=2)
+    {(4, 9), (4, 1), (1, 4), (9, 4)}
+    >>> create_ngram_set([1, 4, 9, 4, 1, 4], ngram_value=3)
+    [(1, 4, 9), (4, 9, 4), (9, 4, 1), (4, 1, 4)]
+    """
+    return set(zip(*[input_list[i:] for i in range(ngram_value)]))
+
+
+
+
+
+
+
+def add_ngram(sequences, token_indice, ngram_range=2):
+    """
+    Augment the input list of list (sequences) by appending n-grams values.
+    Example: adding bi-gram
+    >>> sequences = [[1, 3, 4, 5], [1, 3, 7, 9, 2]]
+    >>> token_indice = {(1, 3): 1337, (9, 2): 42, (4, 5): 2017}
+    >>> add_ngram(sequences, token_indice, ngram_range=2)
+    [[1, 3, 4, 5, 1337, 2017], [1, 3, 7, 9, 2, 1337, 42]]
+    Example: adding tri-gram
+    >>> sequences = [[1, 3, 4, 5], [1, 3, 7, 9, 2]]
+    >>> token_indice = {(1, 3): 1337, (9, 2): 42, (4, 5): 2017, (7, 9, 2): 2018}
+    >>> add_ngram(sequences, token_indice, ngram_range=3)
+    [[1, 3, 4, 5, 1337, 2017], [1, 3, 7, 9, 2, 1337, 42, 2018]]
+    """
+    new_sequences = []
+    for input_list in sequences:
+        new_list = input_list[:]
+        for ngram_value in range(2, ngram_range + 1):
+            for i in range(len(new_list) - ngram_value + 1):
+                ngram = tuple(new_list[i:i + ngram_value])
+                if ngram in token_indice:
+                    new_list.append(token_indice[ngram])
+        new_sequences.append(new_list)
+
+    return new_sequences
+
+
+
+def load_dataset(ngram_range,
+	maxlen):
+	"""
+	"""
+	train_data = 'data/delicious/train-data.dat'
+	train_labels = 'data/delicious/train-label.dat'
+	val_data = 'data/delicious/valid-data.dat'
+	val_labels = 'data/delicious/valid-label.dat'
+	test_data = 'data/delicious/test-data.dat'
+	test_labels = 'data/delicious/test-label.dat'
+	vocab_file = 'data/delicious/vocabs.txt'
+
+
+	print('Loading data...')
+	X_train, y_train = read_data(train_data,train_labels)
+	X_val, y_val = read_data(val_data,val_labels)
+	X_test, y_test = read_data(test_data,test_labels)
+	print(len(X_train), 'train sequences')
+	print(len(X_test), 'test sequences')
+	print('Average train sequence length: {}'.format(np.mean(list(map(len, X_train)), dtype=int)))
+	print('Average test sequence length: {}'.format(np.mean(list(map(len, X_test)), dtype=int)))
+
+
+	word_index = {}
+	with open(vocab_file,'r') as vf:
+		for line in vf:
+			line = line.strip().split(', ')
+			key = line[0]
+			value = int(line[1])
+			word_index[key] = value
+
+	max_features = len(word_index)
+
+	if ngram_range > 1:
+	    print('Adding {}-gram features'.format(ngram_range))
+	    # Create set of unique n-gram from the training set.
+	    ngram_set = set()
+	    for input_list in X_train:
+	        for i in range(2, ngram_range + 1):
+	            set_of_ngram = create_ngram_set(input_list, ngram_value=i)
+	            ngram_set.update(set_of_ngram)
+
+	    # Dictionary mapping n-gram token to a unique integer.
+	    # Integer values are greater than max_features in order
+	    # to avoid collision with existing features.
+	    start_index = max_features + 1
+	    token_indice = {v: k + start_index for k, v in enumerate(ngram_set)}
+	    indice_token = {token_indice[k]: k for k in token_indice}
+
+	    # max_features is the highest integer that could be found in the dataset.
+	    max_features = np.max(list(indice_token.keys())) + 1
+
+	    # Augmenting x_train and x_test with n-grams features
+	    X_train = add_ngram(X_train, token_indice, ngram_range)
+	    X_val = add_ngram(X_val, token_indice, ngram_range)
+	    X_test = add_ngram(X_test, token_indice, ngram_range)
+	    print('Average train sequence length: {}'.format(np.mean(list(map(len, X_train)), dtype=int)))
+	    print('Average val sequence length: {}'.format(np.mean(list(map(len, X_val)), dtype=int)))
+	    print('Average test sequence length: {}'.format(np.mean(list(map(len, X_test)), dtype=int)))
+	
+
+	print('Pad sequences (samples x time)')
+	X_train = sequence.pad_sequences(X_train, maxlen=maxlen)
+	X_val = sequence.pad_sequences(X_val, maxlen=maxlen)
+	X_test = sequence.pad_sequences(X_test, maxlen=maxlen)
+	print('X_train shape:', X_train.shape)
+	print('X_val shape:', X_val.shape)
+	print('X_test shape:', X_test.shape)
+
+
+	return X_train,y_train,X_val,y_val,X_test,y_test,word_index
---
+++ multilabel.py
@@ -0,0 +1,161 @@
+from delicious_loader import load_dataset
+
+
+import numpy as np
+from sklearn.metrics import precision_recall_fscore_support, roc_auc_score
+
+
+from keras.models import Model, model_from_json
+from keras.layers import Dense, Input, Embedding, GlobalAveragePooling1D
+from keras.callbacks import EarlyStopping, ModelCheckpoint, Callback
+
+import tensorflow as tf
+
+
+
+
+
+
+def f1_score(y_true, y_pred):
+	"""
+	Compute the micro f(b) score with b=1.
+	"""
+	y_true = tf.cast(y_true, "float32")
+	y_pred = tf.cast(tf.round(y_pred), "float32") # implicit 0.5 threshold via tf.round
+	y_correct = y_true * y_pred
+
+
+	sum_true = tf.reduce_sum(y_true, axis=1)
+	sum_pred = tf.reduce_sum(y_pred, axis=1)
+	sum_correct = tf.reduce_sum(y_correct, axis=1)
+
+
+	precision = sum_correct / sum_pred
+	recall = sum_correct / sum_true
+	f_score = 2 * precision * recall / (precision + recall)
+	f_score = tf.where(tf.is_nan(f_score), tf.zeros_like(f_score), f_score)
+
+
+	return tf.reduce_mean(f_score)
+
+
+
+
+
+
+def build_model(num_features,
+	num_classes,
+	embedding_dims,
+	maxlen):
+	"""
+	"""
+
+	input_layer = Input(shape=(maxlen,),
+		dtype='int32')
+
+
+	embeddings = Embedding(num_features,
+		embedding_dims,
+		input_length=maxlen)(input_layer)
+
+	avg_layer = GlobalAveragePooling1D()(embeddings)
+	predictions = Dense(num_classes, activation='sigmoid')(avg_layer)
+
+	model = Model(inputs=input_layer, outputs=predictions)
+	model.compile(loss='binary_crossentropy',
+              optimizer='adam',
+              metrics=[f1_score])
+
+	model.summary()
+
+	return model
+
+
+
+
+
+
+def load_model():
+	"""
+	"""
+
+	json_file = open('multilabel_model.json', 'r')
+	loaded_model_json = json_file.read()
+	json_file.close()
+	model = model_from_json(loaded_model_json)
+
+	model.load_weights('multilabel_model.h5')
+	print("Loaded model from disk")
+
+	model.summary()
+
+	model.compile(loss='binary_crossentropy',
+		optimizer='adam',
+		metrics=[f1_score])
+
+
+	return model
+
+
+
+
+
+
+
+if __name__ == '__main__':
+
+	ngram_range = 1
+	maxlen = 200
+	batch_size = 32
+	embedding_dims = 50
+	epochs = 500
+	num_classes = 20 
+
+
+
+	X_train,y_train,X_val,y_val,X_test,y_test,word_index = load_dataset(ngram_range,maxlen)
+
+	num_features = len(word_index)
+	print('Found %d words' % num_features)
+
+
+	model = build_model(num_features,num_classes,embedding_dims,maxlen)
+
+	model_json = model.to_json()
+	with open("multilabel_model.json", "w") as json_file:
+		json_file.write(model_json)
+
+
+	early_stopping =EarlyStopping(monitor='val_f1_score',
+		patience=15,
+		mode='max')
+	bst_model_path = 'multilabel_model.h5'
+	model_checkpoint = ModelCheckpoint(bst_model_path,
+		monitor='val_f1_score',
+		verbose=1,
+		save_best_only=True,
+		mode='max',
+		save_weights_only=True)
+
+	model.fit(X_train, y_train,
+		batch_size=batch_size,
+		epochs=epochs,
+		validation_data=(X_val, y_val),
+		callbacks=[model_checkpoint,early_stopping])
+
+
+	model = load_model()
+	y_pred = model.predict(X_test)
+
+	print 'AUC:',roc_auc_score(y_test, y_pred)
+
+	y_pred[y_pred > 0.5] = 1
+	y_pred[y_pred <= 0.5] = 0
+	
+	for i in range(10):
+		pred,lab = y_pred[i],y_test[i]
+		print np.where(pred == 1), np.where(lab == 1)
+    	
+
+ 	print precision_recall_fscore_support(y_test, y_pred, average='micro')
+	print precision_recall_fscore_support(y_test, y_pred, average='macro')
